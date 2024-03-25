5de531840508bae3f8ad75628972b067dd2209ee
Jmboy713 <jimin713@naver.com>
2024-03-25 20:36:23 +0900

    feat : 엔티티 매핑

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -12,39 +12,16 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         //code
-        try{
-            // 생성
-//            Member member1 = new Member(150L,"A");
-//            Member member2 = new Member(160L,"B");
-//            em.persist(member1);
-//            em.persist(member2); // jdbc batchsize를 써서 한번에 처리할 수 있다. -> 성능 최적화
-                Member member = em.find(Member.class,150L);
-                member.setName("ZZZZZ");
+        try {
 
-//            member.setId(1L);
-//            member.setName("HelloA");// JPA는 트랜잭션 안에서 실행되어야 한다.
-//            em.persist(member);
-
-            // 조회
-//            Member findMember = em.find(Member.class, 1L);// pk로 조회
-//            System.out.println("findMember.id = "+findMember.getId());
-//            System.out.println("findMember.name = "+findMember.getName());//1차 캐시에 저장되어있었어서 hibernate가 select 쿼리를 날리지 않음.
-//            // 삭제
-////            em.remove(findMember);
-//            // 수정
-////            findMember.setName("HelloJPA");
-////            em.persist(findMember); -> 저장하지 않아도 된다.
-//
-//
-
-            System.out.println("-----------------------------");
             tx.commit();
-        }catch(Exception e){
+        } catch (Exception e) {
             tx.rollback();
-        }finally{
+        } finally {
             em.close();
         }
 
+//
         emf.close();
     }
 }
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,37 +1,29 @@
 package hellojpa;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
+import java.util.Date;
 
 @Entity
 @Table(name = "MEMBER")// 이름이 다를 경우 다음과 같이 매핑 가능
 public class Member {
     @Id // pk 를 알려줌.
     private Long id;
-    private String name;
+    @Column(name = "name",insertable = true,updatable = false,nullable = false,length = 10) // DB에는 name
+    private String username; // columnDefinition 은 데이터베이스 컬럼 정보를 직접 줄 수 있다.
+    private Integer age;
+    @Enumerated(EnumType.STRING) // ENUM 타입을 사용할 때
+    private RoleType roleType;
+    @Temporal(TemporalType.TIMESTAMP) // temporal을 사용할 때 date, time , timestamp
+    private Date createdDate; // 날짜를 사용할 때
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+    @Lob // varchar 이상의 큰 크기의 데이터를 넣을 때.
+    private String description;
 
+    @Transient // 컬럼에 매핑시키지 않음.
+    private int temp;
     public Member() {
     }
 
-    public Member(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
