6a213a0b042b58280cc73a47fbfef25aba96cf21
Jmboy713 <jimin713@naver.com>
2024-03-28 14:00:58 +0900

    feat : mappedSuperClass

--- src/main/java/hellojpa/BaseEntity.java
+++ src/main/java/hellojpa/BaseEntity.java
@@ -6,6 +6,6 @@ import jakarta.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
 @MappedSuperclass // mapping 정보만 받는.
-public class BaseEntity {
+public abstract class BaseEntity {
     private LocalDateTime createdDate;
 }
